7fea5b5b56f545d2412d07f0c367d44e405d4d84
Snah-s <camiloernestosoto@gmail.com>
2024-05-22 01:31:17 -0500

    🚀 Set databse

--- database.py
+++ database.py
@@ -2,9 +2,18 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://admin:Ut3c9208@database-1.c4lofrzrpfew.us-east-1.rds.amazonaws.com:3306/api_players"
-# SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"  # Cambia esto a tu URL de base de datos
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+DOCKER_MYSQL_HOST = "54.221.47.71"  
+DOCKER_MYSQL_PORT = 3306
+
+
+MYSQL_DATABASE = "players"
+MYSQL_USER = "my_user"
+MYSQL_PASSWORD = "my_password"
+
+SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{DOCKER_MYSQL_HOST}:{DOCKER_MYSQL_PORT}/{MYSQL_DATABASE}"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 Base = declarative_base()
