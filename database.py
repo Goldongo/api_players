0885f6f33caa32ea58fe2b64ef34da09fe95c44a
Snah-s <camiloernestosoto@gmail.com>
2024-05-21 21:16:03 -0500

    🚑️ Critical database problems

--- database.py
+++ database.py
@@ -2,10 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-IP_ADDRESS = "54.221.47.71"
-PORT = "3306"
-
-SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://usuario:contraseña@{IP_ADDRESS}:{PORT}/db_players"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"  # Cambia esto a tu URL de base de datos
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
