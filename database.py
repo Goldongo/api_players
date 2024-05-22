a87776885a58c36c5064a85c6ed53b0bd9288ed5
Snah-s <camiloernestosoto@gmail.com>
2024-05-22 00:07:22 -0500

    🚀 Set database

--- database.py
+++ database.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"  # Cambia esto a tu URL de base de datos
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://admin:Ut3c9208@database-1.c4lofrzrpfew.us-east-1.rds.amazonaws.com:3306/api_players"
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"  # Cambia esto a tu URL de base de datos
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
